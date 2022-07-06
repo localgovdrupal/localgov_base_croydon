7efbf6951b2ad457ebd9a9bc4d48601a516fe4b7
chris stevens <cjs78@hotmail.co.uk>
2022-07-06 10:28:26 +0100

    fix indentation

--- js/main.js
+++ js/main.js
@@ -19,22 +19,21 @@
         /* Homepage only - services list show hide */
         if (homeExists) {
 
-        var $servicesBlock = $(".layout--threecol-33-34-33");
-        
-        $servicesBlock.slice(2).hide();
-
-        $('.services-show').on('click', function(e) {
-          $(this).removeClass("d-flex").addClass("d-none"); // work around for BS4 d-flex using !important and overriding usual hide method
-          $servicesBlock.show();
-          e.preventDefault();
-        });
-
-        $('.services-hide').on('click', function(e) {
+          var $servicesBlock = $(".layout--threecol-33-34-33");
+          
           $servicesBlock.slice(2).hide();
-          $('.services-show').removeClass("d-none").addClass("d-flex"); // work around for BS4 d-flex using !important and overriding usual hide method
-        });
-        }
 
+          $('.services-show').on('click', function(e) {
+            $(this).removeClass("d-flex").addClass("d-none"); // work around for BS4 d-flex using !important and overriding usual hide method
+            $servicesBlock.show();
+            e.preventDefault();
+          });
+
+          $('.services-hide').on('click', function(e) {
+            $servicesBlock.slice(2).hide();
+            $('.services-show').removeClass("d-none").addClass("d-flex"); // work around for BS4 d-flex using !important and overriding usual hide method
+          });
+        }
       })
     }
   }
